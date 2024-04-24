96a7150f71ce2caed89da04a4ffd6b1551e9f407
WindSoilder <WindSoilder@outlook.com>
2024-04-24 15:45:35 +0800

    fix test

--- crates/nu-command/tests/commands/ls.rs
+++ crates/nu-command/tests/commands/ls.rs
@@ -240,7 +240,7 @@ fn list_files_from_two_parents_up_using_multiple_dots() {
 
         let actual = nu!(
             cwd: dirs.test().join("foo/bar"),
-            "ls ... | sort-by name | get name.0"
+            r#"ls ... | sort-by name | get name.0 | str replace '\\' '/'"#
         );
         assert_eq!(actual.out, "../../andres.xml");
     })
